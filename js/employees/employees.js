4fd502f8409431b314db4cc5419000ab84f398c1
Fakermaker <1pho2trung@gmail.com>
2024-07-17 17:00:04 +0700

    fix controls

--- js/employees/employees.js
+++ js/employees/employees.js
@@ -46,17 +46,31 @@ window.onload = function () {
             fetch('https://cukcuk.manhnv.net/api/v1/Employees').then(res => res.json()).then(data => {
                 console.log(data);
                 let table = document.querySelector('#tableEmp');
+                let i = 1;
                 for (const item of data) {
                     let tr = document.createElement('tr');
-                    tr.innerHTML = `<td>15</td>
+                    tr.className = 'test';
+                    tr.innerHTML = `<td>${i}</td>
                                     <td>${item.EmployeeCode}</td>
                                     <td>${item.FullName}</td>
                                     <td>${item.Gender == 1 ? 'Nam' : 'Nữ'}</td>
                                     <td>${item.DateOfBirth}</td>
                                     <td>${item.Email}</td>
-                                    <td>${item.Address}</td>`
-                                    ;
+                                    <td>${item.Address}
+                                        <div class="table-action">
+                                            <a href="#">
+                                                <img src="./assets/icon/add.png" alt="" class="icon">
+                                            </a>
+                                            <a href="#">
+                                                <img src="./assets/icon/document-online.png" alt="" class="icon">
+                                            </a>
+                                            <a href="#">
+                                                <img src="./assets/icon/delete-48.png" alt="" class="icon">
+                                            </a>
+                                        </div>
+                                    </td>`;
                     table.querySelector('tbody').append(tr);
+                    i++;
                 }
             })
         } catch(error) {
